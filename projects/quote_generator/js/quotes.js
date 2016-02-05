1c62a1fa664844c5c8513ae4ece47094ee311d35
Shaklin Syed <shaklin_s@yahoo.com>
2016-02-05 08:55:22 +0530

    intial version complete

--- projects/quote_generator/js/quotes.js
+++ projects/quote_generator/js/quotes.js
@@ -1,75 +1,96 @@
-var current = 0;
+var current = Math.round(Math.random() * 10);
 var quotes_list = [
 	{
-		url : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Gandhi_smiling.jpg/377px-Gandhi_smiling.jpg",
 		quote : "Happiness is when what you think, what you say, and what you do are in harmony.",
 		author : "Mahatma Gandhi",
 	},
 	{
-		url : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Buddha-little.statue.jpg/194px-Buddha-little.statue.jpg",
 		quote : "Ardently do today what must be done. Who knows? Tomorrow, death comes.",
 		author : "Gautama Buddha",
 	},
 	{
-		url : " ",
 		quote : "Live the life you love and love the life you live.",
 		author : "Bob Marley",
 	},
 	{
-		url : " ",
 		quote : "Life is a gift horse in my opinion.",
 		author : "J. D. Salinger"
 	},
 	{
-		url : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Isaac.Asimov01.jpg/331px-Isaac.Asimov01.jpg",
 		quote : "It pays to be obvious, especially if you have a reputation for subtlety.",
 		author : "Isaac Asimov",
 	},
 	{
-		url : " ",
 		quote : "The world is changing: I feel it in the water, I feel it in the earth, and I smell it in the air.",
 		author : "J. R. R. Tolkien",
 	},
 	{
-		url : " ",
 		quote : "When the sword is once drawn, the passions of men observe no bounds of moderation.",
 		author : "Alexander Hamilton",
 	},
 	{
-		url : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Robert_E_Lee_%28color%29.jpg/202px-Robert_E_Lee_%28color%29.jpg",
 		quote : "It is well that war is so terrible — lest we should grow too fond of it.",
 		author : "Robert E. Lee",
 	},
 	{
-		url : " ",
 		quote : "Uncertainty and expectation are the joys of life. Security is an insipid thing.",
 		author : "William Congreve",
+	},
+	{
+		quote : "Love what you do and do what you love",
+		author: "Steve Jobs",
+	},
+	{
+		quote : "The Journey of one thousand miles begins with one step",
+		author : "Lao Tsu"
+	},
+	{
+		quote : "Life is like riding a bicycle. To keep your balance you must keep moving.",
+		author : "Albert-Einstein",
+	},
+	{
+		quote : "I never think of the future. It comes soon enough.",
+		author : "Albert-Einstein",
+	},
+	{
+		quote : "Only a life lived for others is a life worthwhile.",
+		author : "Albert-Einstein",
+	},
+	{
+		quote : "Perfection of means and confusion of goals seem—in my opinion—to characterize our age.",
+		author : "Albert-Einstein",
+	},
+	{
+		quote : "we are busy that we may have leisure, and make war that we may live in peace.",
+		author : "Aristotle",
 	},
 	
 ];
 
+function composeTweet(){
+	var twtbtn = document.getElementById("twtbtn");
+	twtbtn.href = "https://twitter.com/intent/tweet?text=" + 
+					quotes_list[current].quote + "-"+
+					 quotes_list[current].author + "&hastags=" +
+					 "quotes" + "&url=" + "http://goo.gl/TaEpJm" ;
+}
+
 function getQuote(){
 	console.log("getQuote");
-
-	var image = document.getElementById("image");
+	
 	var quote = document.getElementById("quote");
 	var author = document.getElementById("author");
-
-	image.style.backgroundImage = "url('" + quotes_list[current].url +"')";
-
-
-	if(quotes_list[current].url == " "){
-		image.style.backgroundImage = "";
-	}
 	
+
+	composeTweet();
 	
 	quote.innerHTML = quotes_list[current].quote;
 	author.innerHTML = "-" + quotes_list[current].author;
 
 	current++;
-	changeTitle();
-	
+	console.log(current,quotes_list.length);
 	if(current == quotes_list.length){
-		current =0;
+		current = 0;
 	}
 }
+
--- projects/quote_generator/js/script.js
+++ projects/quote_generator/js/script.js
@@ -1,20 +1,12 @@
-function changeTitle(){
-	var quote = document.getElementById("quote").innerHTML;
-	var title = document.getElementsByTagName("title")[0].innerHTML;
-	
-	document.getElementsByTagName("title")[0].innerHTML = quote + "Quotes";
-}
-
 document.addEventListener("DOMContentLoaded",function(){
 	console.log("Ready");
 
 	var refresh = document.getElementById("refresh");
 
 	refresh.addEventListener("click",getQuote);
-
+	
 	function init(){
 		getQuote();
-		changeTitle();
 	}
 	
 	init();
