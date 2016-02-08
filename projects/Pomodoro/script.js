a823960d465a99afb7925a1fe2e96ef61f10b534
Shaklin Syed <shaklin_s@yahoo.com>
2016-02-08 19:42:23 +0530

    pomodoro clock prototype completed

--- projects/Pomodoro/script.js
+++ projects/Pomodoro/script.js
@@ -13,6 +13,10 @@ document.addEventListener("DOMContentLoaded",function(){
 	var brePlus = document.getElementById("brePlus");
 	var breMin = document.getElementById("breMin");
 
+	var min = document.getElementById("min");
+	var sec = document.getElementById("sec");
+
+	var sesInfo = document.getElementById("sesInfo");
 
 	function incDur(num){
 		return ++num;
@@ -25,19 +29,33 @@ document.addEventListener("DOMContentLoaded",function(){
 	}
 
 	function incSes(){
-		sesLen.innerHTML = incDur(Number(sesLen.innerHTML));
+		if(!timeOut){
+			console.log(timeOut);
+			sesLen.innerHTML = incDur(Number(sesLen.innerHTML));
+			min.innerHTML = sesLen.innerHTML;
+		}
 	}
 	
 	function decSes(){
-		sesLen.innerHTML = decDur(Number(sesLen.innerHTML));
+		if(!timeOut){
+			console.log(timeOut);
+			sesLen.innerHTML = decDur(Number(sesLen.innerHTML));
+			min.innerHTML = sesLen.innerHTML;
+		}
 	}
 	
 	function incBre(){
-		breLen.innerHTML = incDur(Number(breLen.innerHTML));
+		if(!timeOut){
+			console.log(timeOut);
+			breLen.innerHTML = incDur(Number(breLen.innerHTML));
+		}
 	}
 
 	function decBre(){
-		breLen.innerHTML = decDur(Number(breLen.innerHTML));
+		if(!timeOut){
+			console.log(timeOut);
+			breLen.innerHTML = decDur(Number(breLen.innerHTML));
+		}
 	}
 
 	sesPlus.addEventListener("click",incSes);
@@ -45,12 +63,96 @@ document.addEventListener("DOMContentLoaded",function(){
 	brePlus.addEventListener("click",incBre);
 	breMin.addEventListener("click",decBre);
 
+	var minutes, seconds;
 
-	function init(){
-		var startTime = new Date(1,1,1,0,0,0);
-		var endTime = new Date(1,1,1,0,Number(sesLen.innerHTML),0);
+	function setTimeInfo(){
+		if(sesInfo.innerHTML == "Session"){
+			minutes = Number(sesLen.innerHTML);
+		}
+		else{
+			minutes = Number(breLen.innerHTML);
+		}
 
-		console.log(startTime,endTime);
+		if(minutes > 1){
+			// if(sec.innerHTML != 0){
+			// 	seconds = minutes * 60 - (60 - Number(sec.innerHTML));
+			// }
+			// else{
+			// 	seconds = minutes * 60 ;
+			// }
+			seconds = minutes * 60 ;
+			--minutes;
+		}
+		else if(minutes == 1){
+			seconds = 59;
+			minutes = 0;
+		}
+		// else{
+		// 	if(sec.innerHTML != 0){
+		// 		seconds = 60 - (60 - Number(sec.innerHTML));
+		// 	}
+		// 	else{
+		// 		minutes = 0;
+		// 		seconds = 59;
+		// 	}
+		// }
+		console.log(minutes, seconds);
 	}
-	start.addEventListener("click",init);
+
+	// function callBreak(){
+	// 	minutes = Number(breLen.innerHTML);
+	// 	seconds = (minutes * 60) - 1;
+	// }
+	function displayTime(){
+		--seconds;
+
+		if(minutes == 0 && seconds == 0){
+			if(sesInfo.innerHTML == "Session"){
+				sesInfo.innerHTML ="Break";
+				setTimeInfo();
+			}
+			else{
+				sesInfo.innerHTML = "Session";
+				setTimeInfo();
+			}
+		}
+
+		if(seconds % 60 == 0){
+			--minutes;
+		}
+		
+		min.innerHTML = minutes;
+		
+		if(seconds % 60 < 10){
+			sec.innerHTML = "0" + seconds % 60;
+		}
+		else{
+			sec.innerHTML = seconds % 60;
+		}
+
+		console.log(minutes,seconds);
+	}
+
+	var timeOut;
+
+	function startTimer(){
+		setTimeInfo();
+		timeOut = setInterval(displayTime,1000);
+	}
+
+	function pauseTimer(){
+		clearInterval(timeOut);
+		timeOut = false;
+		console.log(timeOut);
+	}
+
+	function resetTimer(){
+		pauseTimer();
+		sesInfo.innerHTML = "Session";
+		min.innerHTML = sesLen.innerHTML;
+		sec.innerHTML = "00";
+	}
+	start.addEventListener("click",startTimer);
+	// pause.addEventListener("click",pauseTimer);
+	reset.addEventListener("click",resetTimer);
 });
